2f1aa8c9eb7647d61156666c3bbaef4065a374d8
zhangxiang <zhangxiang@yunzhangfang.com>
2025-08-19 16:08:01 +0800

    feat: 实现全0子数组数目计算功能
    添加计算全0子数组数目的算法实现，使用双指针方法遍历数组并统计连续0的子数组数量

--- medium.js
+++ medium.js
@@ -3,12 +3,28 @@
  */
 
 /**
- * 306. 累加数
- * @param {string} num
- * @return {boolean}
+ * 2348. 全 0 子数组的数目
+ * 给你一个整数数组 nums ，返回全部为 0 的 子数组 数目。
+ * 子数组 是一个数组中一段连续非空元素组成的序列。
+ * @param {number[]} nums
+ * @return {number}
  */
-var isAdditiveNumber = function (num) {
-
+var zeroFilledSubarray = function(nums) {
+    let ans = 0;
+    let left = 0;
+    let right = 0;
+    const len = nums.length;
+    while (right < len) {
+        if (nums[right] === 0) {
+            right++;
+        } else {
+            ans += (right - left) * (right - left + 1) / 2;
+            left = right + 1;
+            right++;
+        }
+    }
+    ans += (right - left) * (right - left + 1) / 2;
+    return ans;
 };
 
-console.log(isAdditiveNumber("199100199"));
+console.log(zeroFilledSubarray([1, 3, 0, 0, 2, 0, 0, 4]));
