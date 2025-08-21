607606c0211fb06d6d97952bc0e245b42ee0eb3b
zhangxiang <zhangxiang@yunzhangfang.com>
2025-08-21 16:54:13 +0800

    feat: 添加统计全1子矩形的算法实现
    实现1504题统计全1子矩形的算法，使用动态规划方法计算所有满足条件的子矩形数量

--- medium.js
+++ medium.js
@@ -9,22 +9,63 @@
  * @param {number[]} nums
  * @return {number}
  */
-var zeroFilledSubarray = function(nums) {
-    let ans = 0;
-    let left = 0;
-    let right = 0;
-    const len = nums.length;
-    while (right < len) {
-        if (nums[right] === 0) {
-            right++;
-        } else {
-            ans += (right - left) * (right - left + 1) / 2;
-            left = right + 1;
-            right++;
-        }
+var zeroFilledSubarray = function (nums) {
+  let ans = 0;
+  let left = 0;
+  let right = 0;
+  const len = nums.length;
+  while (right < len) {
+    if (nums[right] === 0) {
+      right++;
+    } else {
+      ans += ((right - left) * (right - left + 1)) / 2;
+      left = right + 1;
+      right++;
     }
-    ans += (right - left) * (right - left + 1) / 2;
-    return ans;
+  }
+  ans += ((right - left) * (right - left + 1)) / 2;
+  return ans;
 };
 
-console.log(zeroFilledSubarray([1, 3, 0, 0, 2, 0, 0, 4]));
+// console.log(zeroFilledSubarray([1, 3, 0, 0, 2, 0, 0, 4]));
+
+/**
+ * 1504. 统计全 1 子矩形
+ * 给你一个 m x n 的二进制矩阵 mat ，请你返回有多少个 子矩形 的元素全部都是 1 。
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSubmat = function (mat) {
+  const m = mat.length,
+    n = mat[0].length;
+  let ans = 0;
+
+  for (let top = 0; top < m; top++) {
+    const a = new Array(n).fill(0);
+    for (let bottom = top; bottom < m; bottom++) {
+      const height = bottom - top + 1;
+
+      let last = -1;
+      for (let j = 0; j < n; j++) {
+        a[j] += mat[bottom][j];
+
+        if (a[j] !== height) {
+          last = j;
+        } else {
+          ans += j - last;
+        }
+      }
+    }
+  }
+
+  return ans;
+};
+
+console.log(
+  "1504. 统计全 1 子矩形",
+  numSubmat([
+    [0, 1, 1, 0],
+    [0, 1, 1, 1],
+    [1, 1, 1, 0],
+  ])
+);
