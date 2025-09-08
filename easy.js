d99b10b56bd1191e87c5de417d7a2cb0bb5ee6b2
zhangxiang <zhangxiang@yunzhangfang.com>
2025-09-08 14:03:41 +0800

    feat: 添加将整数转换为两个无零整数的和的功能
    实现1317题的功能，通过遍历找到满足条件的两个无零整数

--- easy.js
+++ easy.js
@@ -117,3 +117,30 @@ var isPowerOfThree = function (n) {
 };
 
 console.log(isPowerOfThree(6));
+
+/**
+ * 1317. 将整数转换为两个无零整数的和
+ * 「无零整数」是十进制表示中 不含任何 0 的正整数。
+ * 给你一个整数 n，请你返回一个 由两个整数组成的列表 [a, b]，满足：
+ * a 和 b 都是无零整数
+ * a + b = n
+ * 题目数据保证至少有一个有效的解决方案。
+ *
+ * 如果存在多个有效解决方案，你可以返回其中任意一个。
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function (n) {
+  let a = 1;
+  let b = n - 1;
+  while (a <= b) {
+    if (!a.toString().includes("0") && !b.toString().includes("0")) {
+      return [a, b];
+    }
+    a++;
+    b--;
+  }
+  return [];
+};
+
+console.log("【1317. 将整数转换为两个无零整数的和】", getNoZeroIntegers(1010));
